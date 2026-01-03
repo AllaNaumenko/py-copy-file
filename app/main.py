312400cdb3e749a1ac330932ecc2f6f222eb805a
Your Name <allanaumenko@Allas-MacBook-Air.local>
2026-01-03 16:25:45 +0800

    Copy_file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3:
+        return
+
+    _, source_file, target_file = parts
+
+    if source_file == target_file:
+        return
+
+    with open(source_file, "r", encoding="utf-8") as file_in, open(
+        target_file, "w", encoding="utf-8"
+    ) as file_out:
+        file_out.write(file_in.read())
