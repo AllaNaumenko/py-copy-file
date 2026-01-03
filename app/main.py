cfb7929f51e97ff9512d13385002c9c951038851
Your Name <allanaumenko@Allas-MacBook-Air.local>
2026-01-03 16:28:39 +0800

    fixed_test

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
+import os
+
+
 def copy_file(command: str) -> None:
     parts = command.split()
-
     if len(parts) != 3:
         return
 
-    _, source_file, target_file = parts
+    cmd, source_file, target_file = parts
+
+    if cmd != "cp":
+        return
 
     if source_file == target_file:
+        return
+
+    if not os.path.exists(source_file):
         return
 
     with open(source_file, "r", encoding="utf-8") as file_in, open(
